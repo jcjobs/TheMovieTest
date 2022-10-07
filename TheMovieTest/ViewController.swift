87915ddae04700e492bc92b756acabe464fefa25
jcjobs <jcarlosp.ceo@gmail.com>
2022-10-07 01:14:42 -0500

    Initial Commit

---
+++ TheMovieTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheMovieTest
+//
+//  Created by Juan Carlos Perez Delgadillo on 07/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
